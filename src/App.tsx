1b868c59c3a070d567c948d56080b1cd27fd8a84
Artyom Sommer <litovskys@gmail.com>
2023-08-04 21:02:51 +0300

    Simple store and logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useListStore } from "./store";
+
 function App() {
+   const { todos, addTodo } = useListStore();
+
    return (
-      <div></div>
+      <div>
+         <button onClick={addTodo}>
+            add todo
+         </button>
+         {todos.map((todo) => <div key={todo.id}>
+            {todo.text}
+         </div>)}
+      </div>
    )
 }
 
---
+++ src/store/index.ts
@@ -0,0 +1,1 @@
+export { default as useListStore } from './useListStore';
